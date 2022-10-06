032fbc64461a707ff8b865252768306df7aa6778
zizfn <1803942+zizifn@users.noreply.github.com>
2022-10-06 22:47:43 +0800

    test nx cloud

--- packages/split-webstreams/src/lib/split-webstreams.spec.ts
+++ packages/split-webstreams/src/lib/split-webstreams.spec.ts
@@ -1,12 +1,12 @@
 import { split } from './split-webstreams';
-import { Readable, PassThrough } from 'stream';
+import { Readable } from 'stream';
 import { createReadStream } from 'fs';
 import { TextDecoderStream } from 'node:stream/web';
 
 describe('splitWebstreams', () => {
   it('should work', async () => {
     //
-    const reader = Readable.toWeb(createReadStream('./package.json')).pipeThrough(new TextDecoderStream()).pipeThrough(split()).getReader()
+    const reader = Readable.toWeb(createReadStream('./nx.json')).pipeThrough(new TextDecoderStream()).pipeThrough(split()).getReader()
     for (let result = await reader.read(); !result.done; result = await reader.read()) {
       console.log('[value]', result.value);
     }

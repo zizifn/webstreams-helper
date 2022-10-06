29a9d41e1a561e44edf89f59d5539065f9b8414b
zizifn <515868058@qq.com>
2022-10-06 22:57:22 +0800

    test nx cloud

--- packages/split-webstreams/src/lib/split-webstreams.spec.ts
+++ packages/split-webstreams/src/lib/split-webstreams.spec.ts
@@ -6,8 +6,15 @@ import { TextDecoderStream } from 'node:stream/web';
 describe('splitWebstreams', () => {
   it('should work', async () => {
     //
-    const reader = Readable.toWeb(createReadStream('./nx.json')).pipeThrough(new TextDecoderStream()).pipeThrough(split()).getReader()
-    for (let result = await reader.read(); !result.done; result = await reader.read()) {
+    const reader = Readable.toWeb(createReadStream('./nx.json'))
+      .pipeThrough(new TextDecoderStream())
+      .pipeThrough(split())
+      .getReader();
+    for (
+      let result = await reader.read();
+      !result.done;
+      result = await reader.read()
+    ) {
       console.log('[value]', result.value);
     }
   });

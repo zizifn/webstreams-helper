3134782d3184b0e3c9aa55e59f57aad6a897202d
zizifn <515868058@qq.com>
2022-10-07 23:30:43 +0800

    switch esm version

--- e2e-test/next-js/next.config.mjs
+++ e2e-test/next-js/next.config.mjs
@@ -3,6 +3,7 @@ import { env } from "./src/env/server.mjs";
 
 /**
  * Don't be scared of the generics here.
+ *
  * All they do is to give us autocompletion when using this.
  *
  * @template {import('next').NextConfig} T

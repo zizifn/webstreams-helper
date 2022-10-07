84d6fb72ab1d63900468f51a16e0ffda24e2bcf4
zizifn <515868058@qq.com>
2022-10-08 00:23:03 +0800

    enhance spilt

--- packages/split-webstreams/src/lib/split-webstreams.spec.ts
+++ packages/split-webstreams/src/lib/split-webstreams.spec.ts
@@ -2,20 +2,25 @@ import { split } from './split-webstreams';
 import { Readable } from 'stream';
 import { createReadStream } from 'fs';
 import { TextDecoderStream } from 'node:stream/web';
+import * as path from 'path';
 
 describe('splitWebstreams', () => {
   it('should work', async () => {
     //
-    const reader = Readable.toWeb(createReadStream('./nx.json'))
+    const reader = Readable.toWeb(
+      createReadStream(path.join(__dirname, 'test-data.txt'))
+    )
       .pipeThrough(new TextDecoderStream())
       .pipeThrough(split())
       .getReader();
+    let final = '';
     for (
       let result = await reader.read();
       !result.done;
       result = await reader.read()
     ) {
-      console.log('[value22 11]', result.value);
+      final += result.value + '\n';
     }
+    console.log(final);
   });
 });
